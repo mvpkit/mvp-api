a050dfcea41ddd9d3a6cc23f6af5cafb0bb665ba
jae lee <jaequery@gmail.com>
2020-09-12 01:02:27 -0700

    - remove dumbo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,8 @@ import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-
   @Get()
-  index(){
-    return "check /swagger for API documentation";
+  index() {
+    return 'check /swagger for API documentation';
   }
-
-
-  @Get('dumb')
-  async dumb() {
-    return `so dummy ${process.env.DUMBO}`
-  }
-
-
 }
